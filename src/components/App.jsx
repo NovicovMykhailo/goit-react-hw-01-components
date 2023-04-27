e62b1e48913f7f73279bac439082aa5139ec78ed
NovicovMykhailo <mikenovicov@gmail.com>
2023-04-27 19:32:09 +0300

    add Transactions

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,11 +3,12 @@ import './app.css';
 import Profile from './profile/profile';
 import StatisticsList from './statistics/Statistics';
 import FriendsList from './friendlist/Friends';
-
+import Transactions from './transactions/Transactions'
 //data ============
 import user from './profile/user.json';
 import statsData from './statistics/data.json';
 import friendsData from './friendlist/friends.json';
+import transactions from './transactions/transactions.json';
 
 // ===============
 export const App = () => {
@@ -22,6 +23,7 @@ export const App = () => {
       />
       <StatisticsList stats={statsData} title="Statistics" />
       <FriendsList friends={friendsData} />
+      <Transactions items={transactions}/>
     </div>
   );
 };
